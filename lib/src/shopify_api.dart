f970f36a5bc67ab37fd118b0f9e93c301191f8c1
vijay_savvient <vijay.savvient@gmail.com>
2023-09-19 16:23:40 +0530

    remove openapi, add http

--- lib/src/shopify_api.dart
+++ lib/src/shopify_api.dart
@@ -1,33 +1,31 @@
-import 'package:api/api.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
+
+
 class ShopifyAPI {
   static String apiKey = "";
   static String apiToken = "";
+  static String url = "";
 
   void initialMethod() {
     debugPrint('APIKey--$apiKey');
     debugPrint('APIToken--$apiToken');
+    debugPrint('API-URL--$url');
   }
 
-  Future<void> callLoginAPI({required String email, required String password, required String deviceId, required String deviceName, required String deviceOs, required String deviceType,
-    required String firebaseToken, required String ip, required String timeZone,}) async {
-    AdminLoginRequest adminLoginRequest =
-    AdminLoginRequest(
-          (b) {
-        b.email = email;
-        b.password = password;
-        b.deviceId = deviceId;
-        b.deviceName = deviceName;
-        b.deviceOs = deviceOs;
-        b.deviceType = deviceType;
-        b.firebaseToken = firebaseToken;
-        b.ip = ip;
-        b.timeZone = timeZone;
-      },
-    );
-    Api().getAdminApi().adminLogin(adminLoginRequest: adminLoginRequest).then((value) {
-      debugPrint('Resposen:- ${value.data?.data?.userDetails.toString()}');
-      debugPrint('Resposen:- ${value.data?.message.toString()}');
-    });
+  Future<void> callLoginAPI({required String apiPath, required Map<String, dynamic>? queryParameters}) async {
+    var url =
+    Uri.https(ShopifyAPI.url, apiPath, queryParameters);
+
+    // Await the http get response, then decode the json-formatted response.
+    var response = await http.post(url);
+    if (response.statusCode == 200) {
+      var jsonResponse =
+      convert.jsonDecode(response.body) as Map<String, dynamic>;
+      print('Login Response: $jsonResponse.');
+    } else {
+      print('Login Response Fail: ${response.statusCode}.');
+    }
   }
 }
